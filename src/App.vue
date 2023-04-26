b0c7634886ee04557124b8222e2897eccf53a1e9
bduha184 <bduha_184@yahoo.co.jp>
2023-04-26 23:07:40 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import MainPage from './components/MainPage.vue'
+import NavigationBar from './components/NavigationBar.vue'
+</script>
+
+<template>
+  <MainPage />
+  <NavigationBar />
+</template>
